2258f82cc28e5c1dccfa6bd8d1d4da48e0eb4573
Ben Carr <ben.carr2@ntlworld.com>
2019-03-15 23:16:06 +0000

    Added a basic scheduler to run everything

--- main.py
+++ main.py
@@ -1,12 +1,53 @@
 from dmx import Universe, RGBAPar
 from ola import OLAInterface
+import asyncio
+import time
 
 
-interface = OLAInterface("http://localhost:9090/set_dmx")
+class Scheduler:
+    def __init__(self, interval, update_cb, loop=None):
+        self.interval = interval
+        self.loop = loop or asyncio.get_event_loop()
+        self.update_cb = update_cb
+        self.last_run = None
+        self.time_bank = 0
+
+    def start(self):
+        self.last_run = time.monotonic() / 1000.0
+        self._schedule_call()
+        self.loop.run_forever()
+
+    def _schedule_call(self):
+        base = time.monotonic()
+        diff = base - round(base)
+        ticks = diff // self.interval
+        next =  round(base) + (ticks + 1) * self.interval
+        self.loop.call_at(next, self._run)
+        self.next_run = next
+
+    def _run(self):
+        print(self.last_run, self.next_run)
+        self.time_bank += self.next_run - self.last_run
+        self.last_run = self.next_run
+
+        while self.time_bank >= self.interval:
+            self._run_tasks()
+            self.time_bank -= self.interval
+
+        self.update_cb()
+        self._schedule_call()
+
+    def _run_tasks(self):
+        pass
+
 
 u = Universe(1, "Test")
 d = RGBAPar(1, "Test")
 u.add(d)
+
+interface = OLAInterface("http://localhost:9090/set_dmx")
+scheduler = Scheduler(1.0/25, lambda: interface.update(u))
+scheduler.start()
 
 import time
 import math
