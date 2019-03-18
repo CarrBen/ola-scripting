8300835844d8a4807cbcb605df3c3632366c2876
Ben Carr <ben.carr2@ntlworld.com>
2019-03-18 22:28:38 +0000

    Add basic light groups to the Studio Stage

--- stages/studio.py
+++ stages/studio.py
@@ -4,7 +4,7 @@ from dmx import RGBAPar
 
 # TODO: Groups of lights
 # TODO: Position information
-u = Universe(1, "Test")
+u = Universe(1, "Studio")
 floor_back_left_corner = RGBAPar(161, "Floor Back Left Corner")
 u.add(floor_back_left_corner)
 grid_back_left = RGBAPar(81, "Grid Back Left")
@@ -27,3 +27,71 @@ floor_back_centre_right = RGBAPar(129, "Floor Back Centre Right")
 u.add(floor_back_centre_right)
 grid_back_left_corner = RGBAPar(65, "Grid Back Left Corner")
 u.add(grid_back_left_corner)
+
+all = (
+    floor_back_left_corner,
+    floor_back_centre_right,
+    floor_back_centre_right,
+    floor_back_right_corner,
+    grid_centre_left,
+    grid_back_left_corner,
+    grid_back_left,
+    grid_back_right,
+    grid_centre_right,
+    grid_front_left,
+    grid_front_right
+)
+
+floor = (
+    floor_back_centre_left,
+    floor_back_centre_right,
+    floor_back_left_corner,
+    floor_back_right_corner
+)
+
+grid = (
+    grid_back_left,
+    grid_back_left_corner,
+    grid_back_right,
+    grid_centre_left,
+    grid_centre_right,
+    grid_front_left,
+    grid_front_right
+)
+
+back = (
+    floor_back_centre_left,
+    floor_back_centre_right,
+    floor_back_left_corner,
+    floor_back_right_corner,
+    grid_back_left,
+    grid_back_left_corner,
+    grid_back_right
+)
+
+centre = (
+    grid_centre_left,
+    grid_centre_right
+)
+
+front = (
+    grid_front_left,
+    grid_front_right
+)
+
+left = (
+    floor_back_centre_left,
+    floor_back_left_corner,
+    grid_back_left,
+    grid_back_left_corner,
+    grid_centre_left,
+    grid_front_left
+)
+
+right = (
+    floor_back_centre_right,
+    floor_back_right_corner,
+    grid_back_right,
+    grid_centre_right,
+    grid_front_right
+)
